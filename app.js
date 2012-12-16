4ea5224ddb1fd03c5cafdba883afc3f49f129571
Ben Sparks <bsparks@unicon.net>
2012-12-15 23:46:57 -0700

    allow cors

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ var express = require('express'),
 
 var app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
